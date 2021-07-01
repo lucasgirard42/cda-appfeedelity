371f2bd7d188a6033494cbe3d203272c9754f709
Girard lucas <pro.lucas.girard@gmail.com>
2021-07-01 16:33:22 +0200

    first

---
+++ src/Controller/CustomerController.php
@@ -0,0 +1,154 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Customer;
+use App\Entity\User;
+use App\Form\CustomerType;
+use App\Repository\CustomerRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Core\User\UserInterface;
+
+/**
+ * @Route("/customer")
+ */
+class CustomerController extends AbstractController
+{
+    /**
+     * @Route("/", name="customer_index", methods={"GET"})
+     */
+    public function index(CustomerRepository $customerRepository): Response
+    {
+
+        /** @var User $user */
+        $user = $this->getUser();
+
+        if (!($user instanceof UserInterface)){
+            return $this->redirectToRoute('app_login');
+        }
+
+        $customer = $user->getCustomers();
+        if (!isset($customer)){
+            $customer = new Customer();
+            return $this->redirectToRoute('customer_new');
+        }
+
+        return $this->render('customer/index.html.twig', [
+            'customers' => $customer,
+        ]);
+        
+        
+    }
+
+    
+
+
+    /**
+     * @Route("/new", name="customer_new", methods={"GET","POST"})
+     */
+    public function new(Request $request): Response
+    {
+        $user = $this-> getUser();
+
+        // Redirect users who already have a customer to edit form
+        if ($user->getCustomers() instanceof Customer){
+            $customerId = $user->getCustomers()->getId();           /////////////// -------------------  ////////////
+            return $this->redirectToRoute('customer_edit',[
+                'id'=> $customerId,
+                'fidelity_point' => 0,
+            ]);
+        }
+
+        $customer = new Customer();
+
+        $form = $this->createForm(CustomerType::class, $customer);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager = $this->getDoctrine()->getManager();
+            
+            $customer->setUser($user);                                  ////// <------------- //////
+
+            $entityManager->persist($customer);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('customer_show_user');
+        }
+
+        return $this->render('customer/new.html.twig', [
+            'customer' => $customer,
+            'form' => $form->createView(),
+        ]);
+    }
+
+    /**
+     * @Route("/", name="customer_show_user", methods={"GET"})
+     */
+    public function showUser(): Response 
+    {
+        /** @var User $user */
+        $user = $this->getUser();
+
+        if (!($user instanceof UserInterface)){
+            return $this->redirectToRoute('app_login');
+        }
+
+        $customer = $user->getCustomers();
+        if (!isset($customer)){
+            $customer = new Customer();
+            return $this->redirectToRoute('customer_new');
+        }
+
+        return $this->render('customer/show.html.twig', [
+            'customer' => $customer,
+        ]);
+    }
+
+    
+    /**
+     * @Route("/{id}", name="customer_show", methods={"GET"})
+     */
+    public function show(Customer $customer): Response
+    {
+        return $this->render('customer/show.html.twig', [
+            'customer' => $customer,
+        ]);
+    }
+
+    /**
+     * @Route("/{id}/edit", name="customer_edit", methods={"GET","POST"})
+     */
+    public function edit(Request $request, Customer $customer): Response
+    {
+        $form = $this->createForm(CustomerType::class, $customer);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $this->getDoctrine()->getManager()->flush();
+
+            return $this->redirectToRoute('customer_index');
+        }
+
+        return $this->render('customer/edit.html.twig', [
+            'customer' => $customer,
+            'form' => $form->createView(),
+        ]);
+    }
+
+    /**
+     * @Route("/{id}", name="customer_delete", methods={"POST"})
+     */
+    public function delete(Request $request, Customer $customer): Response
+    {
+        if ($this->isCsrfTokenValid('delete'.$customer->getId(), $request->request->get('_token'))) {
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->remove($customer);
+            $entityManager->flush();
+        }
+
+        return $this->redirectToRoute('customer_index');
+    }
+}
---
+++ src/Entity/Customer.php
@@ -0,0 +1,211 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\CustomerRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=CustomerRepository::class)
+ */
+class Customer
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\ManyToOne(targetEntity=User::class, inversedBy="customers")
+     */
+    private $user;
+
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    private $firstName;
+
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    private $lastName;
+
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    private $address;
+
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    private $email;
+
+    /**
+     * @ORM\Column(type="integer", nullable=true)
+     */
+    private $phone;
+
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    private $society;
+
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    private $image;
+
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    private $city;
+
+    /**
+     * @ORM\Column(type="integer", nullable=true)
+     */
+    private $fidelityPoint;
+
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    private $service;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getUser(): ?User
+    {
+        return $this->user;
+    }
+
+    public function setUser(?User $user): self
+    {
+        $this->user = $user;
+
+        return $this;
+    }
+
+    public function getFirstName(): ?string
+    {
+        return $this->firstName;
+    }
+
+    public function setFirstName(?string $firstName): self
+    {
+        $this->firstName = $firstName;
+
+        return $this;
+    }
+
+    public function getLastName(): ?string
+    {
+        return $this->lastName;
+    }
+
+    public function setLastName(?string $lastName): self
+    {
+        $this->lastName = $lastName;
+
+        return $this;
+    }
+
+    public function getAddress(): ?string
+    {
+        return $this->address;
+    }
+
+    public function setAddress(?string $address): self
+    {
+        $this->address = $address;
+
+        return $this;
+    }
+
+    public function getEmail(): ?string
+    {
+        return $this->email;
+    }
+
+    public function setEmail(?string $email): self
+    {
+        $this->email = $email;
+
+        return $this;
+    }
+
+    public function getPhone(): ?int
+    {
+        return $this->phone;
+    }
+
+    public function setPhone(?int $phone): self
+    {
+        $this->phone = $phone;
+
+        return $this;
+    }
+
+    public function getSociety(): ?string
+    {
+        return $this->society;
+    }
+
+    public function setSociety(?string $society): self
+    {
+        $this->society = $society;
+
+        return $this;
+    }
+
+    public function getImage(): ?string
+    {
+        return $this->image;
+    }
+
+    public function setImage(?string $image): self
+    {
+        $this->image = $image;
+
+        return $this;
+    }
+
+    public function getCity(): ?string
+    {
+        return $this->city;
+    }
+
+    public function setCity(?string $city): self
+    {
+        $this->city = $city;
+
+        return $this;
+    }
+
+    public function getFidelityPoint(): ?int
+    {
+        return $this->fidelityPoint;
+    }
+
+    public function setFidelityPoint(?int $fidelityPoint): self
+    {
+        $this->fidelityPoint = $fidelityPoint;
+
+        return $this;
+    }
+
+    public function getService(): ?string
+    {
+        return $this->service;
+    }
+
+    public function setService(?string $service): self
+    {
+        $this->service = $service;
+
+        return $this;
+    }
+}
